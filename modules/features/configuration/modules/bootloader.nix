9fa39fc85542089393b2bf79e569325eaec8f652
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-08-30 12:47:16 +0500

    init: architectural refactor

--- modules/features/configuration/modules/bootloader.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }: {
-    boot = {
-        loader.systemd-boot.enable = true;
-        loader.efi.canTouchEfiVariables = true;
-        kernelPackages = pkgs.linuxPackages_latest;
-        tmp.cleanOnBoot = true;
-    };
-}
