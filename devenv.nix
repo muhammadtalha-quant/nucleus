fc95012f7a5d001af2d1b07ef137a7c427f4d9ae
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-09-03 15:48:15 +0500

    devenv: add necessary tools for markdown and nix

--- devenv.nix
+++ devenv.nix
@@ -1,6 +1,19 @@
-{ pkgs, lib, config, inputs, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
 
 {
+  packages = with pkgs; [
+    statix
+    nixfmt
+    marksman
+    markdownlint-cli2
+    markdown-toc
+  ];
   languages.nix = {
     enable = true;
     lsp.package = pkgs.nil;
