c4e42bba3a5c9fe5029cf5bf7464afca0513dc8a
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-09-13 20:36:24 +0500

    kde: remove incompatible pinentry package

--- kde/modules/features/workstation/modules/programs.nix
+++ kde/modules/features/workstation/modules/programs.nix
@@ -1,10 +1,9 @@
-{ pkgs, configDirectory, ... }:
+{ configDirectory, ... }:
 {
   programs = {
     kdeconnect.enable = true;
     nh.flake = configDirectory;
     gnupg.agent = {
-      pinentryPackage = pkgs.pinentry-gnome3;
       settings = {
         default-cache-ttl = 43200;
         max-cache-ttl = 43200;
