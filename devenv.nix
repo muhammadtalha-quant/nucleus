37acddfa0371222a2816d26ce4a3ab73cb387211
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-09-26 11:57:30 +0500

    devenv: add pkgs.treefmt and scripts.lint and scripts.format

--- devenv.nix
+++ devenv.nix
@@ -10,6 +10,7 @@
   packages = with pkgs; [
     statix
     nixfmt
+    nixfmt-tree
   ];
   languages.nix = {
     enable = true;
@@ -19,5 +20,7 @@
     test-minimal.exec = "cd minimal && nix flake check .";
     test-gnome.exec = "cd gnome && nix flake check .";
     test-kde.exec = "cd kde && nix flake check .";
+    lint.exec = "statix check";
+    format.exec = "treefmt --ci *";
   };
 }
